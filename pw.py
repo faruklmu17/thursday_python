a6f418cf7eb9c574f4ee6de40a6717641e10e226
pixilfallslol <yayitstoday22@gmail.com>
2025-03-27 20:48:09 -0500

    lol

--- pw.py
+++ pw.py
@@ -1,6 +1,6 @@
 from playwright.sync_api import sync_playwright
 with sync_playwright() as p:
-    browser = p.chromium.launch(headless=True)
+    browser = p.chromium.launch(headless=False)
     page = browser.new_page()
     page.goto("https://google.com")
     print(page.title())
